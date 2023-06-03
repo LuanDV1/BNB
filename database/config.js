ca1c335701e2830ae7365fb2903e738d337d90c6
luan <vanluan4440@gmail.com>
2023-06-04 00:18:22 +0700

    fix connect DB

--- database/config.js
+++ database/config.js
@@ -1,9 +1,8 @@
 const configDB = {
-  // host:"postgres://bnb_user_admin:qKXNgQx8p8dNmpBLwCsD9ZngzOA7SDJl@dpg-cht28t2k728qeg205is0-a/bnb",
-  user: "bnb_user_admin",
-  password: "qKXNgQx8p8dNmpBLwCsD9ZngzOA7SDJl",
-  database: "bnb",
-  port: 5432,
+  connectionString:'postgres://bnb_user_admin:qKXNgQx8p8dNmpBLwCsD9ZngzOA7SDJl@dpg-cht28t2k728qeg205is0-a.singapore-postgres.render.com/bnb',
+  ssl: {
+      rejectUnauthorized: true
+  }
 };
 
 module.exports = configDB;
--- database/connect.js
+++ database/connect.js
@@ -1,31 +1,26 @@
-const { Client } = require("pg");
+const { Pool } = require('pg');
 const config = require("./config")
-const client = new Client(config);
-
-client
-  .connect()
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((e) => {
-    console.log(e);
-    console.log("Error connecting to database");
-  });
+const itemsPool = new Pool(config);
+itemsPool.connect().then((data)=>{
+  console.log('connected DB');
+}).catch(err=>{
+  console.log('error connect DB');
+})
 
 const insertData = async (query)=>{
-    sqlResult = await client.query(query);
+    sqlResult = await itemsPool.query(query);
     return sqlResult
 }
 const updateData = async (query)=>{
-    sqlResult = await client.query(query);
+    sqlResult = await itemsPool.query(query);
     return sqlResult
 }
 const deleteData = async (query)=>{
-    sqlResult = await client.query(query);
+    sqlResult = await itemsPool.query(query);
     return sqlResult
 }
 const selectData = async (query)=>{
-    sqlResult = await client.query(query);
+    sqlResult = await itemsPool.query(query);
     return sqlResult
 }
 
